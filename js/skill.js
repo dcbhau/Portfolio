955929c05a354872d5fda200f9c750958b8f3570
DC <dhruv.chauhan2711@gmail.com>
2021-11-13 13:58:03 +0530

    recent work started

--- js/skill.js
+++ js/skill.js
@@ -1,24 +1,21 @@
 var flag = true;
 var offsetTop = $("#skill").offset().top;
-      $(document).on("scroll", function () {
-        var height = $(window).height();
-        if ($(window).scrollTop() + height - 100> offsetTop) {
-          $(".skill-per").each(function () {
-            var $this = $(this);
-            var per = $this.attr("per");
-            $this.html(per + "%");
-            if(flag)
-            {
-                $this.delay(500).animate(
-                    {
-                      width: per + "%",
-                    },
-                    1500
-                  );
-                  
-            }
-            
-          });
-          flag=false;
-        }
-      });
+$(document).on("scroll", function () {
+  var height = $(window).height();
+  if ($(window).scrollTop() + height - 100 > offsetTop) {
+    $(".skill-per").each(function () {
+      var $this = $(this);
+      var per = $this.attr("per");
+      $this.html(per + "%");
+      if (flag) {
+        $this.delay(500).animate(
+          {
+            width: per + "%",
+          },
+          1500
+        );
+      }
+    });
+    flag = false;
+  }
+});
